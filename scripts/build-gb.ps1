726cb39a892a1bda133ea55b541c4d818e39bbff
mateusdigital <hello@mateus.digital>
2024-04-01 19:08:43 -0300

    Build assets conditionaly

--- scripts/build-gb.ps1
+++ scripts/build-gb.ps1
@@ -31,29 +31,31 @@ $MDGB_DIR  = "libs/mdgb";
 $GBDK_DIR  = "${MDGB_DIR}/libs/gbdk";
 $BUILD_DIR = "build/gb";
 
+$BUILD_ASSETS = $false;
 
+if($BUILD_ASSETS) {
+    ##--------------------------------------------------------------------------
+    ## Make tiles image from the Photoshop source.
+    psd-to-png --input-path "$RES_DIR/tiles.psd" --output-path "$RES_DIR/tiles.png";
 
-##------------------------------------------------------------------------------
-## Make tiles image from the Photoshop source.
-psd-to-png --input-path "$RES_DIR/tiles.psd" --output-path "$RES_DIR/tiles.png";
+    ##--------------------------------------------------------------------------
+    ## Create the Gameboy Tile data from the image.
+    & ${TOOLS_DIR}/png2gbtiles.exe "${RES_DIR}/tiles.png" -csource -pal="${RES_DIR}/pal" "game_tiles";
 
-##------------------------------------------------------------------------------
-## Create the Gameboy Tile data from the image.
-& ${TOOLS_DIR}/png2gbtiles.exe "${RES_DIR}/tiles.png" -csource -pal="${RES_DIR}/pal" "game_tiles";
+    New-Item  -Force -ItemType Directory "${GAME_DIR}/resources/";
+    Move-Item -Force ./game_tiles* "${GAME_DIR}/resources/";
 
-New-Item  -Force -ItemType Directory "${GAME_DIR}/resources/";
-Move-Item -Force ./game_tiles* "${GAME_DIR}/resources/";
+    ##--------------------------------------------------------------------------
+    ## Create GameBoy Map information from the Tiled.
+    tiled-to-map --input-path "${RES_DIR}/TestMap.tmx"    --output-path "${GAME_DIR}/resources/TestMap";
+    tiled-to-map --input-path "${RES_DIR}/MenuMap.tmx"    --output-path "${GAME_DIR}/resources/MenuMap";
+    tiled-to-map --input-path "${RES_DIR}/CreditsMap.tmx" --output-path "${GAME_DIR}/resources/CreditsMap";
+    tiled-to-map --input-path "${RES_DIR}/HudWin.tmx"     --output-path "${GAME_DIR}/resources/HudWin";
 
-##------------------------------------------------------------------------------
-## Create GameBoy Map information from the Tiled.
-tiled-to-map --input-path "${RES_DIR}/TestMap.tmx"    --output-path "${GAME_DIR}/resources/TestMap";
-tiled-to-map --input-path "${RES_DIR}/MenuMap.tmx"    --output-path "${GAME_DIR}/resources/MenuMap";
-tiled-to-map --input-path "${RES_DIR}/CreditsMap.tmx" --output-path "${GAME_DIR}/resources/CreditsMap";
-tiled-to-map --input-path "${RES_DIR}/HudWin.tmx"     --output-path "${GAME_DIR}/resources/HudWin";
+    ##--------------------------------------------------------------------------
+    New-Item -Force -ItemType Directory "$BUILD_DIR";
 
-##------------------------------------------------------------------------------
-New-Item -Force -ItemType Directory "$BUILD_DIR";
-
+}
 ## Compile the game.
 & "${GBDK_DIR}/bin/lcc "         `
     -I"${GAME_DIR}" -I"${MDGB_DIR}" `
